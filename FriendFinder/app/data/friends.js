83f6555772bf942905fc3f9e47127ccfcf767143
David Thompson <drthusker@gmail.com>
2018-02-10 08:47:11 -0700

    update

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,11 @@
+var friends = [ {
+    "name" : "Dave",
+    "photo" : "http://www.mens-hairstylists.com/wp-content/uploads/2015/10/stylish-haircuts-for-men.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Melina",
+    "photo" : "http://science-all.com/images/wallpapers/girl-pictures/girl-pictures-23.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+},
+
+module.exports = friends;
