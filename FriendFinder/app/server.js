9204f057a80e158544d5274556f624945c850436
David Thompson <drthusker@gmail.com>
2018-02-14 19:39:02 -0700

    port fix

--- FriendFinder/app/server.js
+++ FriendFinder/app/server.js
@@ -11,7 +11,7 @@ var path = require('path');
 var friends = require('../app/data/friends.js');
 
 var app = express();
-var PORT = process.env.PORT || 3000; 
+var port = process.env.PORT || 3000; 
 
 
 app.use(express.static('app/public'));
